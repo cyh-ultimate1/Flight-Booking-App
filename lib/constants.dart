68c5e88e392749419da78d9c2d551f88b8bf7cf9
yoongheng <yoongheng.chan@anacle.com>
2022-08-14 16:59:57 +0800

    updates

--- lib/constants.dart
+++ lib/constants.dart
@@ -18,21 +18,6 @@ class GlobalConstants {
   static const jwt = "jwt";
   static const SessionKey = "SessionKey";
 
-  // static MaterialColor APPPRIMARYCOLOUR_SWATCH = MaterialColor(
-  //   0x0030A0,
-  //   <int, Color>{
-  //     50: Color.fromRGBO(136, 14, 79, .1),
-  //     100: Color.fromRGBO(136, 14, 79, .2),
-  //     200: Color.fromRGBO(136, 14, 79, .3),
-  //     300: Color.fromRGBO(136, 14, 79, .4),
-  //     400: Color.fromRGBO(136, 14, 79, .5),
-  //     500: Color.fromRGBO(136, 14, 79, .6),
-  //     600: Color.fromRGBO(136, 14, 79, .7),
-  //     700: Color.fromRGBO(136, 14, 79, .8),
-  //     800: Color.fromRGBO(136, 14, 79, .9),
-  //     900: Color.fromRGBO(136, 14, 79, 1),
-  //   },
-  // );
   static LinearGradient customLinearGradient = LinearGradient(
     colors: <Color>[
       generateMaterialColor(APPPRIMARYCOLOUR!).shade700,
